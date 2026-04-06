51deed988c78914e7d6ec21bcc73b8b5140120c0
SamuelGHarris <87394175+s-a-h-a@users.noreply.github.com>
2026-04-05 22:22:09 -0600

    Starting to build out climbing card

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	// import DisplayBodyWeight from '$components/body_weight/DisplayBodyWeight.svelte';
-	import Dashboard from '$components/Dashboard.svelte';
+	import { IconCliffJumping } from '@tabler/icons-svelte';
+
 	import type { PageData } from './$types';
 
 	type Props = {
@@ -11,12 +11,21 @@
 </script>
 
 <div class="flex h-full w-screen flex-col items-center justify-center">
-	<div class="w-full flex-1"></div>
+	<div class="flex w-full flex-1 flex-col p-2 sm:w-4/5 lg:w-1/2">
+		<div class="bg-base-300 border-base-content/25 flex h-40 w-full flex-col rounded-sm border p-2">
+			<div class="flex w-full">
+				<div
+					class="text-primary font-doto border-base-content/25 flex items-center justify-center gap-1 rounded-sm border p-1 text-2xl leading-0 font-black"
+				>
+					<IconCliffJumping size={22} />
+					CLIMB
+				</div>
+			</div>
+		</div>
+	</div>
 	<div class="pb-8">
 		<button class="btn btn-lg btn-primary font-doto text-2xl font-black uppercase"
-			>Start Session</button
+			>New Session</button
 		>
 	</div>
-	<!-- <DisplayBodyWeight /> -->
-	<!-- <Dashboard class="h-full w-full" bodyWeightData={[]} /> -->
 </div>
